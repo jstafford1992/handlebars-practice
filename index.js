b0c4f521334bd58c9984fcc2aab5b2023d95f654
Charles Stafford <j.stafford1992@gmail.com>
2017-01-06 12:48:32 -0700

    listen and port setup

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, function(){
+  console.log("App is listening on port: ", port);
+});
 
 
 
 
 //
+module.exports = app;
